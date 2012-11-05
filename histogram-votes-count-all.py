96951d3683c4c071a51020f3c33f122dfafa2ad9
kingdomik <dnsk@mail.ru>
2012-11-05 09:36:12 -0800

    Скрипт переведен на UTF-8

--- histogram-votes-count-all.py
+++ histogram-votes-count-all.py
@@ -1,25 +1,29 @@
-# coding=cp1251
+# coding=utf-8
 
-import sys;
-import string;
+import os
+import sys
+import string
+import codecs
 
 mmm = set()
 for line in open('mmm-candidates.csv'):
     (id, name) = line.split(';')
-    mmm.add(str((int(id) - 1))) # Сдвиг номера кандидатов - Артефакт протокола
+    mmm.add(str((int(id) - 1))) # РЎРґРІРёРі РЅРѕРјРµСЂР° РєР°РЅРґРёРґР°С‚РѕРІ - РђСЂС‚РµС„Р°РєС‚ РїСЂРѕС‚РѕРєРѕР»Р°
 
 limits = range(32, 40)
 histogram = { i : { j : 0 for j in limits } for i in range(0,46) } 
 
 f = open('protocol.csv')
-f.readline() # Пропускаем заголовок протокола
+f.readline() # РџСЂРѕРїСѓСЃРєР°РµРј Р·Р°РіРѕР»РѕРІРѕРє РїСЂРѕС‚РѕРєРѕР»Р°
 for line in f:
     (id, phone, all_votes) = line.split(';')
-    votes = set(all_votes.strip().split(',')) # Убираем дупликаты - Артефакт протокола
+    votes = set(all_votes.strip().split(',')) # РЈР±РёСЂР°РµРј РґСѓРїР»РёРєР°С‚С‹ - РђСЂС‚РµС„Р°РєС‚ РїСЂРѕС‚РѕРєРѕР»Р°
     for limit in limits:
         if len(mmm & votes) < limit:
             histogram[len(votes)][limit] += 1 
 
-print 'Количество голосов;' + ';'.join(str(x) for x in limits)
+f = codecs.open('histogram-votes-count-all.csv', 'w', 'cp1251')
+f.write((u'РљРѕР»РёС‡РµСЃС‚РІРѕ РіРѕР»РѕСЃРѕРІ;' + ';'.join(str(x) for x in limits) + '\n'))
 for key in sorted(histogram.keys()):
-    print str(key) + ';' + ';'.join(str(histogram[key][limit]) for limit in limits)
+    f.write(str(key) + ';' + ';'.join(str(histogram[key][limit]) for limit in limits) + '\n')
+f.close()
